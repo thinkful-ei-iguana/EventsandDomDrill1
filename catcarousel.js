b50fe9471003f13e1733556f6f599fa62f1646d5
Kyle Jensen <kylenjensen19@gmail.com>
2019-10-24 09:06:13 -0700

    Finished first exercise

---
+++ catcarousel.js
@@ -0,0 +1,9 @@
+function changeThumbnail(){
+$('.thumbnail').on('click', function(event) {
+    const imgSrc = $(event.currentTarget).find("img").attr("src");
+    const imgAlt = $(event.currentTarget).find("img").attr("alt");
+    $(".hero img").attr('src', imgSrc).attr('alt', imgAlt);
+  });
+
+}
+$(changeThumbnail());

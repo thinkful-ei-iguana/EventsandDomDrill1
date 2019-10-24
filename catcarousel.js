3426ebd65870f47c1643ebc25aa1e7734bba4a2c
Kyle Jensen <kylenjensen19@gmail.com>
2019-10-24 11:22:43 -0700

    made changes to catcarousel

--- catcarousel.js
+++ catcarousel.js
@@ -1,10 +1,9 @@
-// eslint-disable-next-line strict
 function changeThumbnail(){
-  $('.thumbnail').on('click', function(event) {
-    const imgSrc = $(event.currentTarget).find('img').attr('src');
-    const imgAlt = $(event.currentTarget).find('img').attr('alt');
-    // eslint-disable-next-line no-undef
-    $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+$('.thumbnail').on('click', function(event) {
+    const sourceImage = $(event.currentTarget).find("img").attr("src");
+    const altImage = $(event.currentTarget).find("img").attr("alt");
+    $(".hero img").attr('src', sourceImage).attr('alt', altImage);
   });
+
 }
 $(changeThumbnail());
